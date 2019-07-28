b7b9eefa0033fe7b2b48a1e3393c0ba94c955faf
Pedro Henrique S. Araújo <henrique-araujo@outlook.com>
2019-07-28 19:52:06 -0300

    adding EntryFormComponent

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { EntriesRoutingModule } from './entries-routing.module';
 import { EntryListComponent } from '../entries/entry-list/entry-list.component';
+import { EntryFormComponent } from '../entries/entry-form/entry-form.component';
 
 
 @NgModule({
-  declarations: [EntryListComponent],
+  declarations: [EntryListComponent, EntryFormComponent],
   imports: [
     CommonModule,
-    EntriesRoutingModule
+    EntriesRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class EntriesModule { }
